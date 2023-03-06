cb63cb93005196a2cbdb17bfe63bd33a64ac545d
csengekulker <k71608883429@ktch.hu>
2023-03-06 10:04:48 +0100

    api methods

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  host:string = environment.apihost
+
+  fetchStudents() {
+    let end = 'students'
+    let url = this.host + end
+
+    return this.http.get<any>(url)
+  }
+
+  fetchScores() {
+    let end = 'scores'
+    let url = this.host + end
+
+    return this.http.get<any>(url)
+  }
 }
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../shared/api.service';
 
 @Component({
   selector: 'app-table',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableComponent implements OnInit {
 
-  constructor() { }
+  constructor(private api: ApiService) { }
 
   ngOnInit(): void {
   }
