a48f06782f19566ea44c126e2e44126763af5590
csengekulker <k71608883429@ktch.hu>
2023-03-06 10:53:20 +0100

    incoming data processed and handled

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -10,7 +10,67 @@ export class TableComponent implements OnInit {
 
   constructor(private api: ApiService) { }
 
-  ngOnInit(): void {
+  students!: any[]
+  scores: any[] = []
+
+  studentScores: StudentScore[] = []
+
+  buildArray() {
+    let array: StudentScore[] = []
+
+    this.students.forEach((std: any) => {
+
+      this.scores.forEach((scr: any) => {
+        if (std.id == scr.studentId) {
+
+          let item: StudentScore = {
+            id: std.id,
+            name: std.name,
+            gender: std.gender,
+            group: std.group,
+            score: scr.score,
+          }
+
+          console.log(item);
+          array.push(item)
+
+        }
+      })
+    });
+
+    return array
   }
 
+  ngOnInit(): void {
+
+    this.api.fetchStudents().subscribe({
+      next: (data: any) => {
+        this.students = data
+
+      },
+      error: (err: any) => {
+        console.log(err)
+      }
+    })
+
+    this.api.fetchScores().subscribe({
+      next: (data: any) => {
+        this.scores = data
+
+        this.studentScores = this.buildArray()
+
+      },
+      error: (err: any) => {
+        console.log(err)
+      }
+    })
+  }
+}
+
+interface StudentScore {
+  id: number,
+  name: string,
+  gender: string,
+  group: string
+  score: number,
 }
